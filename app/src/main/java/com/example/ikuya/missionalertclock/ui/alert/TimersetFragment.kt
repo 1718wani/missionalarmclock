3163e05d4e3b9cec7c62a8f473b6fa73af3aed47
im1293 <ikuya1293@gmail.com>
2020-11-26 18:34:09 +0900

    Add
    Timer周りのXmlファイルを追加しました

--- app/src/main/java/com/example/ikuya/missionalertclock/ui/alert/TimersetFragment.kt
+++ app/src/main/java/com/example/ikuya/missionalertclock/ui/alert/TimersetFragment.kt
@@ -1,20 +1,15 @@
 package com.example.ikuya.missionalertclock.ui.alert
 
-import android.app.Dialog
-import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TimePicker
 import androidx.annotation.Nullable
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import com.example.ikuya.missionalertclock.R
-import com.example.ikuya.missionalertclock.camera.TakePhotoActivity
+import com.example.ikuya.missionalertclock.alarm.DuringAlarmSetClock
 import java.util.*
 
 
@@ -35,74 +30,15 @@ class TimersetFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val appButton: Button = view.findViewById(R.id.timerstartbtn)
         appButton.setOnClickListener {
-            val intent = Intent(activity, TakePhotoActivity::class.java)
+            val intent = Intent(activity, DuringAlarmSetClock::class.java)
             startActivity(intent)
         }
     }
 
+    fun showTimePickerDialog(v: View) {
+        val newFragment = TimePickerFragment()
+        newFragment.show(supportFragmentManager, "timePicker")
 
-
-    class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            // Use the current time as the default values for the picker
-
-            val c = Calendar.getInstance()
-            val hour = c.get(Calendar.HOUR_OF_DAY)
-            val minute = c.get(Calendar.MINUTE)
-
-            // Create a new instance of TimePickerDialog and return it
-            return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
-        }
-
-        override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-            // Do something with the time chosen by the user
-        }
     }
 
-
-
-
-
-
-
-
-
 }
-
-//class Tab01Fragment : Fragment() {
-//    private var cnt = 0
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_tab01, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        val args = arguments
-//        if (args != null) {
-//            val count = args.getInt("Counter")
-//            cnt = count + 1
-//        }
-//        val appButton: Button = view.findViewById(R.id.Button)
-//        appButton.setOnClickListener(View.OnClickListener {
-//            val intent = Intent(activity, AppComDetailsActivity::class.java)
-//            startActivity(intent)
-//        })
-//    }
-//
-//    companion object {
-//            fun newInstance(count: Int): Tab01Fragment {
-//                //Fragment インスタンス化
-//                val tab01Fragment = Tab01Fragment()
-//
-//                //Bundleにパラメータを設定
-//                val args = Bundle()
-//                args.putInt("Counter", count)
-//                tab01Fragment.arguments = args
-//                return tab01Fragment
-//            }
-//    }
-//}
